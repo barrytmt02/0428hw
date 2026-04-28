584368fec531b4d0f8fcf8a1458f1fa5e6f7dbfb
Barrytmt02 <lordkaepaiyunkai@gmail.com>
2026-04-28 22:44:42 +0800

    練習

--- MyJS.js
+++ MyJS.js
@@ -1,1 +1,14 @@
-console.log("")
+var num = [];
+
+for (var i = 1; i <= 49; i++) {
+    num.push(i);
+}
+
+for (var i = num.length - 1; i > 0; i--) {
+    var rand = Math.floor(Math.random() * (i + 1));
+    [num[i], num[rand]] = [num[rand], num[i]];
+}
+
+var lotto = num.slice(0, 6);
+
+console.log(lotto);
